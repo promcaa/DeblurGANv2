d2f831c904d41b0aedbf3aef101413497a81d70d
promcaa <124491801+promcaa@users.noreply.github.com>
2023-02-12 09:49:39 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -135,7 +135,7 @@ def get_files():
 
 
 if __name__ == '__main__':
-    img_path=glob('/kaggle/working/Project/low/train/A/*.png')
+    img_path=glob('/kaggle/working/Project/high/test/A/*.png')
     for i in img_path:
         main(i)
    
